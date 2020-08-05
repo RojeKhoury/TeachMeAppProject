51794be28483980330c5eede5309f861eccf82bd
RojeKhoury <42047357+RojeKhoury@users.noreply.github.com>
2020-08-05 22:38:23 +0300

    My first Commit

---
+++ app/src/main/java/com/example/teachmeapp/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.teachmeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity
+{
+    private Button m_loginButton,m_signUpButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        //Creating listener to Login button
+        m_loginButton= (Button) findViewById(R.id.LoginButton);
+        m_loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                OpenLogin();
+            }
+        });
+    }
+
+    public void OpenLogin()
+    {
+        Intent intent = new Intent(this, Login.class);
+        startActivity(intent);
+
+    }
+}
