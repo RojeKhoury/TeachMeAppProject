f108fb2d96cc8c390055859217cc47e92cf033a2
abedghandour <abedghandour198@gmail.com>
2020-09-10 19:25:02 +0300

    bug fix

--- app/src/main/java/com/example/teachmeapp/ScheduleExpandingButton.java
+++ app/src/main/java/com/example/teachmeapp/ScheduleExpandingButton.java
@@ -6,23 +6,31 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ScheduleExpandingButton extends AppCompatActivity {
-        // TODO put data into the vaules below from database
-    ImageView imageView = findViewById(R.id.ScheduleExpanding_ImageView);
-    TextView textViewSubject = findViewById(R.id.ScheduleExpanding_TextView_Subject);
-    TextView textViewLevel = findViewById(R.id.ScheduleExpanding_TextView_Level);
-    TextView textViewTimeStart = findViewById(R.id.ScheduleExpanding_TextView_TimeStart_Value);
-    TextView textViewTimeEnd = findViewById(R.id.ScheduleExpanding_TextView_TimeEnd_Value);
-    TextView textViewAddress = findViewById(R.id.ScheduleExpanding_TextView_Address);
+
+    ImageView imageView;
+    TextView textViewSubject;
+    TextView textViewLevel;
+    TextView textViewTimeStart;
+    TextView textViewTimeEnd;
+    TextView textViewAddress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule_expanding_button);
+
+        // TODO put data into the vaules below from database
+
+        imageView = findViewById(R.id.ScheduleExpanding_ImageView);
+        textViewSubject = findViewById(R.id.ScheduleExpanding_TextView_Subject);
+        textViewLevel = findViewById(R.id.ScheduleExpanding_TextView_Level);
+        textViewTimeStart = findViewById(R.id.ScheduleExpanding_TextView_TimeStart_Value);
+        textViewTimeEnd = findViewById(R.id.ScheduleExpanding_TextView_TimeEnd_Value);
+        textViewAddress = findViewById(R.id.ScheduleExpanding_TextView_Address);
     }
 
     public void OnClick_schedule_expanding_button_back(View view) {
