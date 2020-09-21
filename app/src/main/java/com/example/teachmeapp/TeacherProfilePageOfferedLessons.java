1cd489e43a2b0884de56356e99a3b73826da27a3
abedghandour <abedghandour198@gmail.com>
2020-09-21 18:51:47 +0300

    xml edited

---
+++ app/src/main/java/com/example/teachmeapp/TeacherProfilePageOfferedLessons.java
@@ -0,0 +1,14 @@
+package com.example.teachmeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TeacherProfilePageOfferedLessons extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teacher_profile_page_offered_lessons);
+    }
+}
